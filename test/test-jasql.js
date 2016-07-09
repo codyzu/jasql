9997ae13210fa627916cf2096c16198d88d7a541
codyzu <cody.zuschlag@gmail.com>
2016-07-09 19:04:43 +0200

    fix tests

--- test/test-jasql.js
+++ test/test-jasql.js
@@ -2,34 +2,52 @@ import test from 'blue-tape'
 import Jasql from '../lib'
 import {isValid} from 'shortid'
 
-test('models', (t) => {
-  const jasql = new Jasql()
+const jasql = new Jasql()
 
-  const doc = {
-    name: 'cody'
-  }
+const doc = {
+  name: 'cody'
+}
 
-  return jasql.initialize()
-    .then(() => jasql.put(doc))
-    .then((actualDoc) => {
-      console.log('NEW DOC:', actualDoc)
-      t.ok(actualDoc.name === doc.name, 'has the name set')
-      t.ok(actualDoc._id, 'has _id set')
-      t.ok(isValid(actualDoc._id), '_id is a valid shortid')
+test('setup', (t) => jasql.initialize())
+
+test('put returns the created document with an added _id', (t) => jasql.put(doc)
+  .then((actualDoc) => {
+    console.log('NEW DOC:', actualDoc)
+    t.ok(actualDoc.name === doc.name, 'has the name set')
+    t.ok(actualDoc._id, 'has _id set')
+    t.ok(isValid(actualDoc._id), '_id is a valid shortid')
+  }))
+
+test('get returns document by id', (t) => {
+  let id
+
+  return jasql
+    .put(doc)
+    .then((d) => {
+      console.log('CREATED DOC', d)
+      return d
     })
-    .then(
-      () => jasql.destroy(),
-      (err) => {
-        return jasql.destroy()
-          .catch((destroyErr) => console.log(`Error on destroy: ${destroyErr}`))
-          .then(() => { throw err })
-      }
-    )
+    .then((actualDoc) => { id = actualDoc._id })
+    .then(() => jasql.get(id))
+    .then((actualDoc) => {
+      t.equal(actualDoc.name, doc.name, 'has the correct name')
+      t.equal(actualDoc._id, id, 'has the correct id')
+    })
 })
 
-// function testJasql(func) {
-//   const jasql = new Jasql()
+test('getAll returns all documents', (t) => {
+  const doc2 = {
+    name: 'brian'
+  }
 
-//   jasql.initialize()
-//     .then
-// }
+  return jasql.put(doc)
+    .then(() => jasql.put(doc2))
+    .then(() => jasql.getAll())
+    .then((docs) => {
+      console.log('DOCS:', docs)
+      t.ok(Array.isArray(docs), 'the results is an array')
+      t.ok(docs.length >= 2, 'there are at least 2 docs')
+    })
+})
+
+test('teardown', (t) => jasql.destroy())
